1f3e76633c9b7f08dd9fc64e67046b2737f61044
Diego-dot-web <134121369+Diego-dot-web@users.noreply.github.com>
2023-11-24 22:48:47 -0500

    Update display default data

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const display = document.querySelector("#display");
 const numbers = document.querySelectorAll("#number");
 const operations = document.querySelectorAll("#operation");
 const equal = document.querySelector("#equal");
+const results = [];
 let num1;
 let num2;
 let symbol;
@@ -10,24 +11,24 @@ let symbol;
 
 // 2. Basic Operations
 function add (num1, num2){
-    return parseInt(num1 + num2)
-}
+    return parseInt(num1 + num2);
+};
 
 function substract (num1, num2){
-    return parseInt(num1 - num2)
-}
+    return parseInt(num1 - num2);
+};
 
 function multiply (num1, num2){
-    return parseInt(num1 * num2)
-}
+    return parseInt(num1 * num2);
+};
 
 function divide (num1, num2){
-    return parseInt(num1 / num2)
-}
+    return parseInt(num1 / num2);
+};
 
 
 //3.  Call certain operator
-function operator (num1, symbol, num2){
+function operator (symbol){
     if (symbol === '+'){
         add(num1, num2)
     } else if (symbol === '-'){
@@ -36,38 +37,44 @@ function operator (num1, symbol, num2){
         multiply(num1, num2)
     } else if (symbol === '-'){
         divide(num1, num2)
-    }
+    };
     
-}
+};
 
 // 5. Update the UI
 function updateUi (number){
-    const value = parseInt(display.textContent += number.textContent)
-    return value
-}
+    value = display.textContent += number.textContent;
+    return value;
+};
 
 // 7. do the math
 function doTheMath(symbol){
-    num1 = parseInt(display.textContent)
-    display.textContent = ''
-    operator(num1, symbol, num2)     
-}
+    num1 = parseInt(display.textContent);
+    results.push(num1);
+    display.textContent = 0;
+    operator(symbol);     
+};
 
 // 6. listen to the operators
 for (let j = 0; j < operations.length; j++) {
     const symbol = operations[j];
-    symbol.addEventListener("click", ()=> doTheMath(symbol)) 
-}
+    symbol.addEventListener("click", ()=> doTheMath(symbol)); 
+};
 
 
 //4. listen to the event of the buttons
 for (let j = 0; j < numbers.length; j++) {
     const number = numbers[j];
-    number.addEventListener("click", ()=> updateUi(number)) 
-}
+    number.addEventListener("click", ()=> {
+        updateUi(number) 
+    });
+};
 
 
 // 1. clear the display
 clear.addEventListener("click", ()=> {
-    display.textContent = "";
+    display.textContent = 0;
 });
+
+
+//usar reduce para obtener el valor en funcion de la operacion, no mostrar el resultado hasta que se escriba el simbolo o igula  
