74aef2f06caba53619cd9fdf834e800bf03026d2
Diego-dot-web <134121369+Diego-dot-web@users.noreply.github.com>
2023-11-25 19:06:05 -0500

    Add storage for two values

--- script.js
+++ script.js
@@ -4,14 +4,22 @@ const numbers = document.querySelectorAll("#number");
 const operations = document.querySelectorAll("#operation");
 const equal = document.querySelector("#equal");
 const results = [];
-let num1;
-let num2;
-let symbol;
 
+let num1 = '';
+let num2 = '';
+let symbol = '';
+display.textContent = 0;
+
+
+// 1. clear the display
+clear.addEventListener("click", ()=> {
+    display.textContent = 0;
+    num2 = '';
+});
 
 // 2. Basic Operations
 function add (num1, num2){
-    return parseInt(num1 + num2);
+    console.log(parseInt(num1) + parseInt(num2)) ;
 };
 
 function substract (num1, num2){
@@ -28,7 +36,7 @@ function divide (num1, num2){
 
 
 //3.  Call certain operator
-function operator (symbol){
+function operator (num1, symbol, num2){
     if (symbol === '+'){
         add(num1, num2)
     } else if (symbol === '-'){
@@ -41,40 +49,33 @@ function operator (symbol){
     
 };
 
-// 5. Update the UI
-function updateUi (number){
-    value = display.textContent += number.textContent;
-    return value;
-};
-
-// 7. do the math
-function doTheMath(symbol){
-    num1 = parseInt(display.textContent);
-    results.push(num1);
-    display.textContent = 0;
-    operator(symbol);     
-};
-
-// 6. listen to the operators
-for (let j = 0; j < operations.length; j++) {
-    const symbol = operations[j];
-    symbol.addEventListener("click", ()=> doTheMath(symbol)); 
-};
-
-
 //4. listen to the event of the buttons
+
 for (let j = 0; j < numbers.length; j++) {
     const number = numbers[j];
     number.addEventListener("click", ()=> {
-        updateUi(number) 
+        num2 += number.textContent
+        updateUi()
     });
 };
 
 
-// 1. clear the display
-clear.addEventListener("click", ()=> {
-    display.textContent = 0;
-});
+// 5. Update the UI
+function updateUi (){
+    display.textContent = num2;
+};
 
+// 6. listen to the operations
+for (let j = 0; j < operations.length; j++) {
+    const operator = operations[j];
+    operator.addEventListener("click", ()=> {
+        symbol = operator.textContent;
+        num1 += num2;
+        results.push(num1);
+        num1= '';
+        num2 = '';
+        updateUi()
+    });
+};
 
 //usar reduce para obtener el valor en funcion de la operacion, no mostrar el resultado hasta que se escriba el simbolo o igula  
