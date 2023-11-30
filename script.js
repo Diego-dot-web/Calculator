b5927374be9d0e45454ae2153b80398137e4a414
Diego-dot-web <134121369+Diego-dot-web@users.noreply.github.com>
2023-11-29 20:35:04 -0500

    Improve logic

--- script.js
+++ script.js
@@ -11,152 +11,99 @@ let num2 = '';
 let symbol = '';
 display.textContent = 0;
 
-
-// 1. clear the display
-clear.addEventListener("click", ()=> {
+// 1. Clear the display
+clear.addEventListener("click", () => {
     display.textContent = 0;
     num2 = '';
     results = [];
+    symbols = [];
 });
 
 // 2. Basic Operations
-function add (num1, num2){
-    num1 = parseInt(results[0]);
-    num2 = parseInt(results[1]);
-    // using fibonacci for operating 
-    for (let i = 2; i <= results.length; i++) {
-        let current = num1 + num2;
-        num2 = num1;
-        num1 = current;
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+// 3. Call certain operator
+function operator(num1, symbol, num2) {
+    num1 = results.length > 0 ? results[0] : 0;
+    num2 = parseFloat(num2);
+
+    if (symbol === '+') {
+        results[0] = add(num1, num2);
+    } else if (symbol === '-') {
+        results[0] = subtract(num1, num2);
+    } else if (symbol === '*') {
+        results[0] = multiply(num1, num2);
+    } else if (symbol === '/') {
+        results[0] = divide(num1, num2);
     }
-    console.log(num1);
-    results.push(num1);
-    results.splice(0,2);
-    symbols.splice(0,1);
 
+    display.textContent = results[0];
+}
 
-    display.textContent = num1;
-};
-
-function substract (num1, num2){
-    num1 = parseInt(results[0]);
-    num2 = parseInt(results[1]);
-    // using fibonacci for operating 
-    for (let i = 2; i <= results.length; i++) {
-        let current = num1 - num2;
-        num2 = num1;
-        num1 = current;
-    }
-    console.log(num1);
-    results.push(num1);
-    results.splice(0,2);
-    symbols.splice(0,1);
-
-    display.textContent = num1;
-};
-
-function multiply (num1, num2){
-    num1 = parseInt(results[0]);
-    num2 = parseInt(results[1]);
-    // using fibonacci for operating 
-    for (let i = 2; i <= results.length; i++) {
-        let current = num1 * num2;
-        num2 = num1;
-        num1 = current;
-    }
-    console.log(num1);
-    results.push(num1);
-    results.splice(0,2);
-    symbols.splice(0,1);
-
-
-    display.textContent = num1;
-};
-
-function divide (num1, num2){
-    num1 = parseInt(results[0]);
-    num2 = parseInt(results[1]);
-    // using fibonacci for operating 
-    for (let i = 2; i <= results.length; i++) {
-        let current = num1 / num2;
-        num2 = num1;
-        num1 = current;
-    }
-    console.log(num1);
-    results.push(num1);
-    results.splice(0,2);
-    symbols.splice(0,1);
-
-
-    display.textContent = num1;
-};
-
-
-//3.  Call certain operator
-function operator (num1, symbol, num2){
-    if (symbol === '+'){
-        add(num1, num2);
-    } else if (symbol === '-'){
-        substract(num1, num2);
-    } else if (symbol === '*'){
-        multiply(num1, num2);
-    } else if (symbol === '/'){
-        divide(num1, num2);
-    };
-    
-};
-
-// 8. Button equals 
-equal.addEventListener("click", ()=> {
-    symbol = symbols[0];
-    num1 += num2;
-    results.push(parseInt(num1));
-    // num1 = '';
-    num2 = num1;
+// 8. Button equals
+equal.addEventListener("click", () => {
+    symbol = symbols.length > 0 ? symbols[0] : '';
+    num1 = results.length > 0 ? results[0] : '';
     operator(num1, symbol, num2);
+    num1 = results[0];
+    symbols = [];
+    num2 = '';
 });
 
-//4. listen to the event of the buttons
+// 4. Listen to the event of the buttons
 for (let j = 0; j < numbers.length; j++) {
     const number = numbers[j];
-    number.addEventListener("click", ()=> {
-        num2 += number.textContent
-        updateUiNumbers()
+    number.addEventListener("click", () => {
+        num2 += number.textContent;
+        updateUiNumbers();
     });
-};
+}
 
-//7. Update UI Numbers
-function updateUiNumbers (){
-    display.textContent = parseInt(num2);
+// 7. Update UI Numbers
+function updateUiNumbers() {
+    display.textContent = Number(num2);
 }
 
 // 5. Update the UI Symbols
-function updateUiSymbols (){
-    if (symbol === ''){
-        display.textContent = parseInt(num2);
-    } else if (symbol != ''){
-        display.textContent = parseInt(results[0])
+function updateUiSymbols() {
+    if (symbol === '') {
+        display.textContent = Number(num2);
+    } else if (symbol !== '') {
+        display.textContent = results[0];
     }
-};
+}
 
-// 6. listen to the operations
+// 6. Listen to the operations
 for (let j = 0; j < operations.length; j++) {
     const operato = operations[j];
-    
-    operato.addEventListener("click", ()=> {
+
+    operato.addEventListener("click", () => {
         symbols.push(operato.textContent);
         symbol = symbols[0];
-        num1 += num2;
-        results.push(parseInt(num1));
-        num1 = '';
+        num1 = results.length > 0 ? results[0] : '';
+        results.push(parseFloat(num2));
         num2 = '';
-        
-        if (results.length >= 2){
-            operator(num1, symbol, num2)
-        } 
-        
-        if (results.length < 2){
-            updateUiSymbols()
+
+        if (results.length >= 2) {
+            operator(num1, symbol, num2);
+        }
+
+        if (results.length < 2) {
+            updateUiSymbols();
         }
     });
-};
+}
